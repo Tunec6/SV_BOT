997eb30c3be406ac4e3016a153daea349ca8daf9
Tunec <vadimstepanov04@gmail.com>
2023-06-16 13:29:14 +0500

    new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf, Markup } = require('telegraf')
 
+
 const bot = new Telegraf('6001849203:AAGkgCToA-BJAMnbxxBjDstVOVGixbpLcpo')
 
 function getRandomInit(min, max) {
@@ -16,6 +17,56 @@ const getCoinSide = () => getRandomInit(0, 1) === 0 ? 'Орел' : 'Решка'
 const coinInlineKeyboard = Markup.inlineKeyboard([
     Markup.button.callback('Подбросить еще раз', 'flip_a_coin')
 ])
+
+
+let studens = ['Yra' , 'ura']
+
+ function addStudens(text) {
+    studens.push(text)
+}
+
+bot.hears('Добавить студента', ctx => {
+    addStudens(ctx.message.studens)
+})
+
+const yesNoKeyboard = Markup.inlineKeyboard([
+        Markup.button.callback('Да', 'yes'),
+        Markup.button.callback('Нет', 'no')
+    ])
+
+bot.on('text', ctx => {
+
+    ctx.replyWithHTML(
+        `Вы действительно хотите добавить этого студента:`+
+        `${ctx.message.text}`,
+        yesNoKeyboard
+    )
+})
+
+bot.action(['yes', 'no'], ctx => {
+    if (ctx.callbackQuery.data === 'yes') {
+        addStudens(ctx.session.taskText)
+        ctx.editMessageText('Ваша задача успешно добавлена')
+    } else {
+        ctx.deleteMessage()
+    }
+})
+
+
+bot.hears('Студенты', ctx => {
+    ctx.session.taskText = ctx.message.text
+
+    ctx.replyWithHTML(
+        'Студенты:' +
+        `${studens}`
+        )
+
+})
+
+bot.hears('Удалить студента', ctx => {
+    ctx.replyWithHTML(`${studens.pop()}`)
+
+})
 
 bot.hears('Подбросить монетку', ctx => ctx.reply(getCoinSide(), coinInlineKeyboard))
 bot.action('flip_a_coin', async (ctx) => {
@@ -34,7 +85,7 @@ bot.action('random_number', async (ctx) => {
 
 bot.use(async (ctx) => {
     await ctx.reply('Что делать?', Markup.keyboard([
-        ['Подбросить монетку', 'Случайное число']
+        ['Подбросить монетку', 'Случайное число', 'Добавить студента', 'Студенты', 'Удалить студента']
     ]).resize()
     )
 })
